723c154fd43d80cc219d4c636c1f56bbdc89913a
Bayu Anta <bayuantadota@gmail.com>
2026-07-26 14:06:30 +0700

    Feat: Add ProtectedRoute guard so dashboard requires authentication

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,29 +1,34 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+/* eslint-disable react/prop-types */
+import React, { useContext } from "react";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import Header from "../InitialPage/Sidebar/Header";
 import Sidebar from "../InitialPage/Sidebar/Sidebar";
 import { pagesRoute, posRoutes, publicRoutes } from "./router.link";
-import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ThemeSettings from "../InitialPage/themeSettings";
-// import CollapsedSidebar from "../InitialPage/Sidebar/collapsedSidebar";
 import Loader from "../feature-module/loader/loader";
-// import HorizontalSidebar from "../InitialPage/Sidebar/horizontalSidebar";
-//import LoadingSpinner from "../InitialPage/Sidebar/LoadingSpinner";
+import { AuthContext } from "../core/context/AuthContext";
+
+const ProtectedRoute = () => {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!user) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  return <Outlet />;
+};
 
 const AllRoutes = () => {
   const data = useSelector((state) => state.toggle_header);
-  // const layoutStyles = useSelector((state) => state.layoutstyledata);
+
   const HeaderLayout = () => (
     <div className={`main-wrapper ${data ? "header-collapse" : ""}`}>
       <Header />
-      {/* {layoutStyles == "collapsed" ? (
-        <CollapsedSidebar />
-      ) : layoutStyles == "horizontal" ? (
-        <HorizontalSidebar />
-      ) : (
-        <Sidebar />
-      )} */}
       <Sidebar />
       <Outlet />
       <ThemeSettings />
@@ -48,35 +53,32 @@ const AllRoutes = () => {
     </div>
   );
 
-  console.log(publicRoutes, "dashboard");
-
   return (
     <div>
       <Routes>
-        <Route path="/pos" element={<Pospages />}>
-          {posRoutes.map((route, id) => (
-            <Route path={route.path} element={route.element} key={id} />
-          ))}
-        </Route>
-        <Route path={"/"} element={<HeaderLayout />}>
-          {publicRoutes.map((route, id) => (
-            <Route path={route.path} element={route.element} key={id} />
-          ))}
-        </Route>
-
+        {/* Auth Pages (Public Access: Signin, Register, etc.) */}
         <Route path={"/"} element={<Authpages />}>
           {pagesRoute.map((route, id) => (
             <Route path={route.path} element={route.element} key={id} />
           ))}
         </Route>
 
-        {/* <Route path={"/expenses/"} element={<HeaderLayout />}>
-          {expensesRoutes.map((route, id) => (
-            <Route path={route.path} element={route.element} key={id} />
-          ))}
-        </Route> */}
+        {/* Protected Routes (Requires Login) */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/pos" element={<Pospages />}>
+            {posRoutes.map((route, id) => (
+              <Route path={route.path} element={route.element} key={id} />
+            ))}
+          </Route>
+          <Route path={"/"} element={<HeaderLayout />}>
+            {publicRoutes.map((route, id) => (
+              <Route path={route.path} element={route.element} key={id} />
+            ))}
+          </Route>
+        </Route>
       </Routes>
     </div>
   );
 };
+
 export default AllRoutes;
